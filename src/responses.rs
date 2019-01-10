3f87d14f5a3f47d04d2dcb6feffd771284867019
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2019-01-10 21:53:31 +0400

    Fix issue: If total records is equal to size, paginated response shows incorrect number of total pages

--- src/responses.rs
+++ src/responses.rs
@@ -24,8 +24,9 @@ impl<T> PaginatedResponse<T> {
             limit = 1;
         }
 
+        let page_count =  (total_records as f64/ limit as f64).ceil() as u32;
+
         let size = data.len() as u32;
-        let page_count = (total_records / limit) + 1u32;
         let last = page >= page_count;
         return PaginatedResponse {
             data: data,
@@ -34,5 +35,65 @@ impl<T> PaginatedResponse<T> {
             page_count: page_count,
             last: last,
         };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_total_records_less_than_limit() {
+        let names : Vec<String> = vec!["james".into(), "bond".into(), "harry".into(), "potter".into(), "ron".into()];
+        let response = PaginatedResponse::new(names, 1u32, 5u32, 10u32);
+    
+        assert_eq!(response.page, 1);
+        assert_eq!(response.size, 5);
+        assert_eq!(response.page_count, 1);
+        assert_eq!(response.last, true);
+    }
+
+    #[test]
+    fn test_total_records_equals_limit() {
+        let names : Vec<String> = vec!["james".into(), "bond".into(), "harry".into(), "potter".into(), "ron".into()];
+        let response = PaginatedResponse::new(names, 1u32, 5u32, 5u32);
+    
+        assert_eq!(response.page, 1);
+        assert_eq!(response.size, 5);
+        assert_eq!(response.page_count, 1);
+        assert_eq!(response.last, true);
+    }
+
+    #[test]
+    fn test_total_records_more_than_limit() {
+        let names : Vec<String> = vec!["james".into(), "bond".into(), "harry".into(), "potter".into(), "ron".into()];
+        let response = PaginatedResponse::new(names, 1u32, 25u32, 10u32);
+    
+        assert_eq!(response.page, 1);
+        assert_eq!(response.size, 5);
+        assert_eq!(response.page_count, 3);
+        assert_eq!(response.last, false);
+    }
+
+    #[test]
+    fn test_last_page_is_true_when_page_is_last() {
+        let names : Vec<String> = vec!["james".into(), "bond".into(), "harry".into(), "potter".into(), "ron".into()];
+        let response = PaginatedResponse::new(names, 3u32, 25u32, 10u32);
+    
+        assert_eq!(response.page, 3);
+        assert_eq!(response.size, 5);
+        assert_eq!(response.page_count, 3);
+        assert_eq!(response.last, true);
+    }
+
+    #[test]
+    fn test_last_page_is_true_when_page_is_last2() {
+        let names : Vec<String> = vec!["james".into(), "bond".into(), "harry".into(), "potter".into(), "ron".into()];
+        let response = PaginatedResponse::new(names, 4u32, 25u32, 10u32);
+    
+        assert_eq!(response.page, 4);
+        assert_eq!(response.size, 5);
+        assert_eq!(response.page_count, 3);
+        assert_eq!(response.last, true);
     }
 }
